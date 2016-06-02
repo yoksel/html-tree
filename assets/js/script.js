651525ac673740c084fa58a9397762bac8e66167
yokselzok <yoksel@yandex.ru>
2016-06-02 14:24:36 +0300

    Add BEM checking

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,21 @@ var tree = doc.querySelector('.gnr-tree');
 var rangeDeep = doc.querySelector('.gnr-deep__range');
 var valDeep = doc.querySelector('.gnr-deep__digit');
 var maxDeep = 1;
-
 var styleElem = doc.createElement('style');
 doc.head.appendChild( styleElem );
 
+// DEV
+var testMarkup = '<ul class="main-nav header__nav"><li class="main-nav__item header__li"><a class="link main-nav__link" href="https://htmlacademy.ru/">HMTL Academy</a></li></ul>';
+codeInput.value = testMarkup;
+createTreeFromHTML ( testMarkup );
+
 codeInput.oninput = function() {
+  createTreeFromHTML ( this.value );
+};
 
-  codeOutput.innerHTML = this.value;
+function createTreeFromHTML ( code ) {
 
-  console.log( 'value.length: ', this.value.length );
+  codeOutput.innerHTML = code;
 
   var items =  makeList( codeOutput, 1 );
 
@@ -28,7 +34,7 @@ codeInput.oninput = function() {
 
   setRange();
 
-};
+}
 
 //------------------------------
 
@@ -55,9 +61,37 @@ function makeList( elem, level ) {
 
   if ( className ) {
 
+    elem.level = level;
+
+    // Check Bem in levels more then firts child
+    if ( level > 2 ) {
+      checkBemNaming ( elem );
+    }
+
     var classSpan = doc.createElement('span');
-    classSpan.classList.add('gnr-elem__class');
-    classSpan.innerHTML = '.' + className;
+    classSpan.classList.add('gnr-elem__class', 'gnr-class');
+
+    for (var i = 0; i < elem.classList.length; i++) {
+      var classItem = elem.classList[ i ];
+      var classItemSpan = doc.createElement('span');
+      classItemSpan.classList.add('gnr-class__item');
+      classItemSpan.innerHTML += classItem;
+
+      // Check valid Bem naiming
+      if ( elem.classList.validBem &&
+           elem.classList.validBem[ classItem ] === false  ) {
+
+        classItemSpan.classList.add('gnr-class__item--warning');
+      }
+
+      classSpan.appendChild( classItemSpan );
+
+      if ( i < elem.classList.length - 1) {
+        classSpan.innerHTML += ' ';
+      }
+    }
+
+    classSpan.innerHTML = '.' + classSpan.innerHTML;
     liContent.appendChild ( classSpan );
   }
 
@@ -65,10 +99,11 @@ function makeList( elem, level ) {
 
   if ( elem.children ) {
 
-    level++;
 
     var childrenList = doc.createElement('ul');
     childrenList.classList.add('gnr-level','gnr-level--' + level);
+
+    level++;
 
     for ( var i = 0; i < elem.children.length; i++ ){
 
@@ -83,6 +118,7 @@ function makeList( elem, level ) {
            && child.tagName != 'BR') {
 
         var newElem = makeList( child, level );
+
         if ( newElem ) {
           childrenList.appendChild( newElem );
         }
@@ -104,6 +140,74 @@ function makeList( elem, level ) {
 
 //------------------------------
 
+function checkBemNaming( elem ) {
+
+  if ( elem.className.indexOf('__') < 0 &&
+       elem.className.indexOf('--') < 0 ) {
+    return;
+  }
+
+  elem.classList['validBem'] = {};
+  var parentPrefixes = findPrefixInParentNode( elem );
+
+  for ( var i = 0; i < elem.classList.length; i++ ) {
+    var classItem = elem.classList[ i ];
+    var prefixCorrect = false;
+    var modifPrefixCorrect = false;
+
+    // Check first part of class with __
+    if ( classItem.split('__').length > 1 ) {
+      var prefix = classItem.split('__')[0];
+
+      if ( elem.parentNode.classList.contains( prefix ) ) {
+        prefixCorrect = true;
+      }
+      else {
+        // Try to find prefix in parent classes prefixes
+        var parentPrefixes = findPrefixInParentNode( elem );
+
+        if ( parentPrefixes[ prefix ] ) {
+          prefixCorrect = true;
+        }
+      }
+
+      elem.classList['validBem'][ classItem ] = prefixCorrect;
+    }
+
+    // Check first part of class with --
+    if ( classItem.split('--').length > 1 ) {
+      var modifPrefix = classItem.split('--')[0];
+
+      if ( elem.classList.contains( modifPrefix ) ) {
+        modifPrefixCorrect = true;
+      }
+
+      elem.classList['validBem'][ classItem ] = modifPrefixCorrect;
+    }
+
+  }
+}
+
+//------------------------------
+
+function findPrefixInParentNode( elem ) {
+  var classList = elem.parentNode.classList;
+  var prefixes = {};
+
+  for (var i = 0; i < classList.length; i++) {
+    var classItem = classList[i];
+
+    if ( classItem.split('__').length > 1 ) {
+      var prefix = classItem.split('__')[0];
+      prefixes[ prefix ] = prefix;
+    }
+  }
+
+  return prefixes;
+}
+
+//------------------------------
+
 function setRange () {
   rangeDeep.max = maxDeep;
   rangeDeep.value = maxDeep;
@@ -113,8 +217,8 @@ function setRange () {
 //------------------------------
 
 rangeDeep.oninput = function () {
-  var level = +this.value + 1;
-  var styles = '.level--' + level + '{ display: none }';
+  var level = +this.value;
+  var styles = '.gnr-level--' + level + '{ display: none }';
   styleElem.innerHTML = styles;
   valDeep.innerHTML = this.value;
 }
