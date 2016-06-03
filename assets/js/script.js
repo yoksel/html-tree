767b0b84669f1ca594c5a65a895add10fe1005ba
yokselzok <yoksel@yandex.ru>
2016-06-03 13:05:59 +0300

    Add checkers for headers and notation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,91 @@
 var doc = document;
 var codeInput = doc.querySelector('.gnr-code-input');
 var codeOutput = doc.querySelector('.gnr-code-output');
-var tree = doc.querySelector('.gnr-tree');
+var treeContent = doc.querySelector('.gnr-tree__content');
+var treePlaceHolder = doc.querySelector('.gnr-tree__placeholder');
 var rangeDeep = doc.querySelector('.gnr-deep__range');
 var valDeep = doc.querySelector('.gnr-deep__digit');
 var maxDeep = 1;
+var message = doc.querySelector('.gnr-message');
+var bemMessage = doc.querySelector('.gnr-message--bem');
+var headersMessage = doc.querySelector('.gnr-message--headers');
+var headersMessageContent = doc.querySelector('.gnr-message--headers .gnr-message__content');
+var headersLevels = {};
+var headersOrder = ['H1','H2','H3','H4','H5','H6'];
+var isWHolePage = false;
+var hasBemWarning = false;
+
+var wholePageMarkers = ['META', 'TITLE', 'LINK'];
+var skippedTags = ['SCRIPT', 'META', 'TITLE', 'LINK', 'NOSCRIPT', 'BR', 'svg'];
+
 var styleElem = doc.createElement('style');
 doc.head.appendChild( styleElem );
 
-// DEV
-var testMarkup = '';
-codeInput.value = testMarkup;
-// createTreeFromHTML ( testMarkup );
+// DEV ONLY
+// runDev();
 
-codeInput.oninput = function() {
+//------------------------------
+
+codeInput.oninput = function () {
+  setHeadersDefs();
+  isWHolePage = false;
+  hasBemWarning = false;
+  maxDeep = 1;
+
+  headersMessage.classList.add( 'gnr-hidden' );
+  bemMessage.classList.add( 'gnr-hidden' );
+
   createTreeFromHTML ( this.value );
 };
 
+//------------------------------
+
+function setHeadersDefs () {
+  headersLevels = {
+    'H1': false,
+    'H2': false,
+    'H3': false,
+    'H4': false,
+    'H5': false,
+    'H6': false
+    };
+}
+
+//------------------------------
+
 function createTreeFromHTML ( code ) {
+
+  if( !code ) {
+    treeContent.classList.add('gnr-hidden');
+    treePlaceHolder.classList.remove('gnr-hidden');
+    setRange();
+    return;
+  }
 
   codeOutput.innerHTML = code;
 
-  var items =  makeList( codeOutput, 1 );
+  var items = makeList( codeOutput, 1 );
 
-  if ( tree.childElementCount > 0 ) {
-    tree.removeChild( tree.firstElementChild );
+  if ( treeContent.childElementCount > 0 ) {
+    treeContent.removeChild( treeContent.firstElementChild );
   }
 
   var list = doc.createElement('ul');
   list.classList.add('gnr-level','gnr-level--0');
   list.appendChild( items );
-  tree.appendChild( list );
+  treeContent.appendChild( list );
+
+  treeContent.classList.remove('gnr-hidden');
+  treePlaceHolder.classList.add('gnr-hidden');
 
   setRange();
 
+  showCodeErrors();
 }
 
 //------------------------------
 
-function makeList( elem, level ) {
+function makeList ( elem, level ) {
 
   var item = doc.createElement('li');
   item.classList.add('gnr-level__item');
@@ -57,6 +104,11 @@ function makeList( elem, level ) {
   tagSpan.classList.add('gnr-elem__tag');
   tagSpan.innerHTML = tagName;
 
+  // Check headers levels
+  if ( headersLevels[ tagName ] !== undefined ) {
+    headersLevels[ tagName ] = true;
+  }
+
   liContent.appendChild ( tagSpan );
 
   if ( className ) {
@@ -65,7 +117,7 @@ function makeList( elem, level ) {
 
     // Check Bem in levels more then firts child
     if ( level > 2 ) {
-      checkBemNaming ( elem );
+      checkBemForElem ( elem );
     }
 
     var classSpan = doc.createElement('span');
@@ -99,7 +151,6 @@ function makeList( elem, level ) {
 
   if ( elem.children ) {
 
-
     var childrenList = doc.createElement('ul');
     childrenList.classList.add('gnr-level','gnr-level--' + level);
 
@@ -109,13 +160,9 @@ function makeList( elem, level ) {
 
       var child = elem.children[ i ];
 
-      if ( child.tagName
-           && child.tagName != 'SCRIPT'
-           && child.tagName != 'META'
-           && child.tagName != 'TITLE'
-           && child.tagName != 'LINK'
-           && child.tagName != 'NOSCRIPT'
-           && child.tagName != 'BR') {
+      checkIsWholePage( child );
+
+      if ( !checkIsSkippedTag( child )) {
 
         var newElem = makeList( child, level );
 
@@ -140,7 +187,7 @@ function makeList( elem, level ) {
 
 //------------------------------
 
-function checkBemNaming( elem ) {
+function checkBemForElem ( elem ) {
 
   if ( elem.className.indexOf('__') < 0 &&
        elem.className.indexOf('--') < 0 ) {
@@ -152,11 +199,10 @@ function checkBemNaming( elem ) {
 
   for ( var i = 0; i < elem.classList.length; i++ ) {
     var classItem = elem.classList[ i ];
-    var prefixCorrect = false;
-    var modifPrefixCorrect = false;
 
     // Check first part of class with __
     if ( classItem.split('__').length > 1 ) {
+      var prefixCorrect = false;
       var prefix = classItem.split('__')[0];
 
       if ( elem.parentNode.classList.contains( prefix ) ) {
@@ -172,10 +218,15 @@ function checkBemNaming( elem ) {
       }
 
       elem.classList['validBem'][ classItem ] = prefixCorrect;
+
+      if ( !prefixCorrect ) {
+        hasBemWarning = true;
+      }
     }
 
     // Check first part of class with --
     if ( classItem.split('--').length > 1 ) {
+      var modifPrefixCorrect = false;
       var modifPrefix = classItem.split('--')[0];
 
       if ( elem.classList.contains( modifPrefix ) ) {
@@ -183,6 +234,10 @@ function checkBemNaming( elem ) {
       }
 
       elem.classList['validBem'][ classItem ] = modifPrefixCorrect;
+
+      if ( !modifPrefixCorrect ) {
+        hasBemWarning = true;
+      }
     }
 
   }
@@ -190,7 +245,7 @@ function checkBemNaming( elem ) {
 
 //------------------------------
 
-function findPrefixInParentNode( elem ) {
+function findPrefixInParentNode ( elem ) {
   var classList = elem.parentNode.classList;
   var prefixes = {};
 
@@ -223,3 +278,118 @@ rangeDeep.oninput = function () {
   valDeep.innerHTML = this.value;
 }
 
+//------------------------------
+
+function showCodeErrors () {
+
+  checkHeadersLevels();
+  showBemMessage();
+
+}
+
+function showBemMessage () {
+
+  if ( hasBemWarning ) {
+    bemMessage.classList.remove( 'gnr-hidden' );
+  }
+  else {
+    bemMessage.classList.add( 'gnr-hidden' );
+  }
+}
+
+//------------------------------
+
+function checkHeadersLevels () {
+  var isWrongOrder = false;
+  var realOrder = doc.createElement('dl');
+  realOrder.classList.add('headers__list');
+  var maxUsedHeaders = 0;
+  var tempHeadersStack = 0;
+  var longestHeadersStack = 0;
+
+  var realOrderDt = doc.createElement('dt');
+  realOrderDt.classList.add('headers__title');
+  realOrderDt.innerHTML = 'Заголовки: ';
+  realOrder.appendChild( realOrderDt );
+
+  for( var key in headersLevels ) {
+      if ( headersLevels[ key ] ) {
+        maxUsedHeaders++;
+        tempHeadersStack++;
+      }
+      else {
+        if ( longestHeadersStack < tempHeadersStack ) {
+          longestHeadersStack = tempHeadersStack;
+        }
+        tempHeadersStack = 0;
+      }
+  }
+
+  if ( maxUsedHeaders > longestHeadersStack ) {
+    isWrongOrder = true;
+  }
+  else if ( isWHolePage && !headersLevels[ 'H1' ] ){
+   isWrongOrder = true;
+  }
+
+  if ( isWrongOrder ) {
+    for (var i = 0; i < headersOrder.length; i++) {
+      var headerItem = headersOrder[i];
+      var headerItemSpan = doc.createElement('dd');
+      headerItemSpan.classList.add('headers__item');
+      headerItemSpan.innerHTML = headerItem;
+
+      if ( headersLevels[ headerItem ] ) {
+        headerItemSpan.classList.add('headers__item--found');
+      }
+      else {
+        headerItemSpan.classList.add('headers__item--notfound');
+      }
+
+      realOrder.appendChild( headerItemSpan );
+    }
+
+    if ( headersMessageContent.firstChild ) {
+      headersMessageContent.removeChild( headersMessageContent.firstChild );
+    }
+    headersMessageContent.appendChild( realOrder );
+  }
+
+  if ( isWrongOrder ) {
+    headersMessage.classList.remove( 'gnr-hidden' );
+  }
+  else {
+    headersMessage.classList.add( 'gnr-hidden' );
+  }
+
+}
+
+//------------------------------
+
+function checkIsSkippedTag ( elem ) {
+
+  if ( skippedTags.indexOf( elem.tagName) >= 0 ) {
+    return true;
+  }
+
+  return false;
+}
+
+//------------------------------
+
+function checkIsWholePage( elem ){
+
+  if ( wholePageMarkers.indexOf( elem.tagName ) >= 0) {
+    isWHolePage = true;
+  }
+}
+
+//------------------------------
+
+function runDev () {
+  var testMarkup = '<section class="pricеs"><h2>Hello</h2><input class="price" type="radio" id="btn-4" name="toggle" checked><input class="price" type="radio" id="btn-5" name="toggle"><input class="price" type="radio" id="btn-6" name="toggle"><h4>Hello</h4><div class="prices__controls"><label class="control" for="btn-4"></label><label class="control" for="btn-5"></label><label class="control" for="btn-6"></label></div></section>';
+  codeInput.value = testMarkup;
+  setHeadersDefs();
+  hasBemWarning = false;
+  createTreeFromHTML ( testMarkup );
+}
